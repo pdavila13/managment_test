29ab020791c580f51ccd22f4bb942ba3bea34a72
Paolo Dávila <pdavila@iesebre.com>
2017-05-11 20:59:31 +0200

    Added from for create and show study

--- resources/views/studies/index.blade.php
+++ resources/views/studies/index.blade.php
@@ -7,11 +7,70 @@
 @section('main-content')
     <div class="container-fluid spark-screen">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Home</div>
+            <div class="col-md-10 col-md-offset-1">
 
-                    <div class="panel-body">
+                <div class="box box-default">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Create studies</h3>
+
+                        <div class="box-tools pull-right">
+                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                            </button>
+                        </div>
+                        <!-- /.box-tools -->
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="alert alert-success alert-dismissible">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                            <h4><i class="icon fa fa-check"></i> Done!</h4>
+                            Studies created OK!
+                        </div>
+
+                        <div class="alert alert-danger alert-dismissible">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                            <h4><i class="icon fa fa-ban"></i> Oops!</h4>
+                            Something is wrong!
+                        </div>
+
+                        <form role="form" action="/studies" method="post">
+                        {{ csrf_field() }}
+                        <!-- text input -->
+                            <input type="hidden" name="law_id" value="1">
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="text" class="form-control" placeholder="Name">
+                            </div>
+
+                            <div class="form-group has-warning">
+                                <label class="control-label" for="inputWarning"><i class="fa fa-bell-o"></i> Input with
+                                    warning</label>
+                                <input type="text" class="form-control" id="inputWarning" placeholder="Name" name="name">
+                                <span class="help-block">Name is required</span>
+                            </div>
+
+                            <input type="submit" value="Create">
+                        </form>
+                    </div>
+                    <!-- /.box-body -->
+
+                    <div class="box-footer">
+                        <button type="submit" class="btn btn-primary">Create</button>
+                    </div>
+                </div>
+
+                <div class="box box-default collapsed-box">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Studies</h3>
+
+                        <div class="box-tools pull-right">
+                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                            </button>
+                        </div>
+                        <!-- /.box-tools -->
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
                         <table class="table table-bordered">
                             <thead>
                             <tr>
@@ -30,6 +89,7 @@
                             </tbody>
                         </table>
                     </div>
+                    <!-- /.box-body -->
                 </div>
             </div>
         </div>
